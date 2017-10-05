447a3d8ef98824f29a3f78d80ea27d78fa767c5a
minh_t57\minht57 <minhtri1996vl@gmail.com>
2017-10-05 23:59:51 +0700

    first commit project

---
+++ Debug/UART/UART.d
@@ -0,0 +1,27 @@
+# FIXED
+
+UART/UART.obj: ../UART/UART.c
+UART/UART.obj: ../UART/UART.h
+UART/UART.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h
+UART/UART.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdbool.h
+UART/UART.obj: D:/ti/TivaWare_C_Series/inc/hw_ints.h
+UART/UART.obj: D:/ti/TivaWare_C_Series/inc/hw_memmap.h
+UART/UART.obj: D:/ti/TivaWare_C_Series/inc/hw_types.h
+UART/UART.obj: D:/ti/TivaWare_C_Series/driverlib/gpio.h
+UART/UART.obj: D:/ti/TivaWare_C_Series/driverlib/interrupt.h
+UART/UART.obj: D:/ti/TivaWare_C_Series/driverlib/pin_map.h
+UART/UART.obj: D:/ti/TivaWare_C_Series/driverlib/sysctl.h
+UART/UART.obj: D:/ti/TivaWare_C_Series/driverlib/uart.h
+
+../UART/UART.c: 
+../UART/UART.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdbool.h: 
+D:/ti/TivaWare_C_Series/inc/hw_ints.h: 
+D:/ti/TivaWare_C_Series/inc/hw_memmap.h: 
+D:/ti/TivaWare_C_Series/inc/hw_types.h: 
+D:/ti/TivaWare_C_Series/driverlib/gpio.h: 
+D:/ti/TivaWare_C_Series/driverlib/interrupt.h: 
+D:/ti/TivaWare_C_Series/driverlib/pin_map.h: 
+D:/ti/TivaWare_C_Series/driverlib/sysctl.h: 
+D:/ti/TivaWare_C_Series/driverlib/uart.h: 
---
+++ Debug/main.d
@@ -0,0 +1,67 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: ../include.h
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdbool.h
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdio.h
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/linkage.h
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdarg.h
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/math.h
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/_defs.h
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/string.h
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdlib.h
+main.obj: D:/ti/TivaWare_C_Series/driverlib/rom_map.h
+main.obj: D:/ti/TivaWare_C_Series/driverlib/udma.h
+main.obj: D:/ti/TivaWare_C_Series/driverlib/ssi.h
+main.obj: D:/ti/TivaWare_C_Series/driverlib/sysctl.h
+main.obj: D:/ti/TivaWare_C_Series/driverlib/gpio.h
+main.obj: D:/ti/TivaWare_C_Series/driverlib/pin_map.h
+main.obj: D:/ti/TivaWare_C_Series/driverlib/timer.h
+main.obj: D:/ti/TivaWare_C_Series/driverlib/interrupt.h
+main.obj: D:/ti/TivaWare_C_Series/driverlib/uart.h
+main.obj: D:/ti/TivaWare_C_Series/driverlib/fpu.h
+main.obj: D:/ti/TivaWare_C_Series/driverlib/rom.h
+main.obj: D:/ti/TivaWare_C_Series/inc/hw_types.h
+main.obj: D:/ti/TivaWare_C_Series/inc/hw_memmap.h
+main.obj: D:/ti/TivaWare_C_Series/inc/hw_gpio.h
+main.obj: D:/ti/TivaWare_C_Series/inc/hw_ints.h
+main.obj: D:/ti/TivaWare_C_Series/inc/hw_timer.h
+main.obj: D:/ti/TivaWare_C_Series/inc/hw_uart.h
+main.obj: D:/ti/TivaWare_C_Series/inc/hw_pwm.h
+main.obj: D:/ti/TivaWare_C_Series/utils/uartstdio.h
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdarg.h
+main.obj: ../UART/UART.h
+
+../main.c: 
+../include.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdbool.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdio.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/linkage.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdarg.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/math.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/_defs.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/string.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdlib.h: 
+D:/ti/TivaWare_C_Series/driverlib/rom_map.h: 
+D:/ti/TivaWare_C_Series/driverlib/udma.h: 
+D:/ti/TivaWare_C_Series/driverlib/ssi.h: 
+D:/ti/TivaWare_C_Series/driverlib/sysctl.h: 
+D:/ti/TivaWare_C_Series/driverlib/gpio.h: 
+D:/ti/TivaWare_C_Series/driverlib/pin_map.h: 
+D:/ti/TivaWare_C_Series/driverlib/timer.h: 
+D:/ti/TivaWare_C_Series/driverlib/interrupt.h: 
+D:/ti/TivaWare_C_Series/driverlib/uart.h: 
+D:/ti/TivaWare_C_Series/driverlib/fpu.h: 
+D:/ti/TivaWare_C_Series/driverlib/rom.h: 
+D:/ti/TivaWare_C_Series/inc/hw_types.h: 
+D:/ti/TivaWare_C_Series/inc/hw_memmap.h: 
+D:/ti/TivaWare_C_Series/inc/hw_gpio.h: 
+D:/ti/TivaWare_C_Series/inc/hw_ints.h: 
+D:/ti/TivaWare_C_Series/inc/hw_timer.h: 
+D:/ti/TivaWare_C_Series/inc/hw_uart.h: 
+D:/ti/TivaWare_C_Series/inc/hw_pwm.h: 
+D:/ti/TivaWare_C_Series/utils/uartstdio.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdarg.h: 
+../UART/UART.h: 
