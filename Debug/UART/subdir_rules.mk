07df7a2420b038119f5751da837a97f1172d0374
minh_t57\minht57 <minhtri1996vl@gmail.com>
2017-10-06 17:56:15 +0700

    Change mother folder name

--- Debug/UART/subdir_rules.mk
+++ Debug/UART/subdir_rules.mk
@@ -8,7 +8,7 @@ SHELL = cmd.exe
 UART/UART.obj: ../UART/UART.c $(GEN_OPTS) | $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM Compiler'
-	"D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="D:/OneDrive/workspace/TIVA_CC1101" --include_path="D:/ti/TivaWare_C_Series" --include_path="D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include" --define=ccs="ccs" --define=PART_TM4C123GH6PM -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="UART/UART.d" --obj_directory="UART" $(GEN_OPTS__FLAG) "$<"
+	"D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="D:/OneDrive/workspace/TIVA_CC1101_UART" --include_path="D:/ti/TivaWare_C_Series" --include_path="D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include" --define=ccs="ccs" --define=PART_TM4C123GH6PM -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="UART/UART.d" --obj_directory="UART" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
